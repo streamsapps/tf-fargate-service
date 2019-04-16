bc5605a7e5b5d4d0e8d387d8ac805bce09a36e21
Barry Tam <barry.tam@capitalone.com>
2019-04-16 17:17:31 +0000

    adding second listener for http to https redirection

--- alb.tf
+++ alb.tf
@@ -39,6 +39,22 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
+resource "aws_lb_listener" "http_to_https" {
+  load_balancer_arn = "${aws_lb.alb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 output "alb_arn" {
   value       = "${aws_lb.alb.arn}"
   description = "arn of alb created"
